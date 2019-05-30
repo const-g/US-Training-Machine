3398b676d3256f4bff6bb9a5fa6857719842d581
Constantin Guay <const.g@gmail.com>
2019-05-30 11:55:39 +0200

    code simplification

--- src/UserStoryTest.php
+++ src/UserStoryTest.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 use PHPUnit\Framework\TestCase;
+use Agile\UserStory;
 
 final class UserStoryTest extends TestCase
 {
     public function testIsUserStory(): void
     {
-        $UserStory = new Agile\UserStory();
-        $this->assertInstanceOf(Agile\UserStory::class, $UserStory);
+        $UserStory = new UserStory();
+        $this->assertInstanceOf(UserStory::class, $UserStory);
     }
 }
